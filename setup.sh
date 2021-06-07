d779a8d514fccd15a00da5fceb99032e0a57b2d0
Masong19hippows <garten323@gmail.com>
2021-06-07 20:22:17 +0100

    Fixing -v option

--- setup.sh
+++ setup.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-echo "Updating and instaling required packages"
-apt-get update && apt-get upgrade -y
-sleep 2
-apt-get install python3-pip python3-pil python3-numpy -y
-sleep 2
-python3 -m pip install RPi.GPIO spidev
-sleep 2
-echo "All Done!"
+
+if  [[ $1 = "-v" ]]; then
+    echo "Option -v turned on"
+    echo "Updating and instaling required packages..."
+    apt-get update && apt-get upgrade -y
+    sleep 2
+    apt-get install python3-pip python3-pil python3-numpy -y
+    sleep 2
+    python3 -m pip install RPi.GPIO spidev
+    sleep 2
+    echo "All Done"
+else
+    echo "Option -v Turned off"
+    echo "Updating and instaling required packages..."
+    apt-get update > /dev/null 2>&1 && apt-get upgrade -y > /dev/null 2>&1
+    sleep 2
+    apt-get install python3-pip python3-pil python3-numpy -y > /dev/null 2>&1
+    sleep 2
+    python3 -m pip install RPi.GPIO spidev > /dev/null 2>&1
+    sleep 2
+    echo "All Done!"
+fi
+    exit
