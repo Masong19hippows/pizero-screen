7117623e4c427642c5fed19b717332a47625353a
Mason Garten <mgarten@sctelcom.com>
2021-06-07 15:16:45 -0500

    Adding setup support

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if  [[ $1 = "-v" ]]; then
     echo "Updating and instaling required packages..."
     apt-get update && apt-get upgrade -y
     sleep 2
-    apt-get install python3-pip python3-pil python3-numpy -y
+    apt-get install python3-pip python3-pil python3-numpy python-binance -y
     sleep 2
     python3 -m pip install RPi.GPIO spidev
     sleep 2
@@ -16,10 +16,17 @@ else
     echo "Updating and instaling required packages..."
     apt-get update > /dev/null 2>&1 && apt-get upgrade -y > /dev/null 2>&1
     sleep 2
-    apt-get install python3-pip python3-pil python3-numpy -y > /dev/null 2>&1
+    apt-get install python3-pip python3-pil python3-numpy python-binance -y > /dev/null 2>&1
     sleep 2
     python3 -m pip install RPi.GPIO spidev > /dev/null 2>&1
     sleep 2
     echo "All Done!"
 fi
-    exit
+
+# Get API key and Secret and add into .env file
+echo "Type in your api key:"
+read apiKey
+echo "Now, type in your api secret:"
+read apiSecret
+
+printf "apiKey=$apiKey\napiSecret=$apiSecret" > .env
