17a9f4c918bffb60fb25426114d9e910605534ce
Masong19hippows <garten323@gmail.com>
2021-06-07 21:45:29 -0500

    Removing unnessesary prints

--- main.py
+++ main.py
@@ -13,12 +13,6 @@ try:
     display = epd2in13b_V3.EPD()
     display.init(display.lut_full_update)
     display.Clear(0) # 0: Black, 255: White
-
-# These valuse are reversed intentionally to make sense physically.
-    w = display.height
-    h = display.width
-    print('width:', w)
-    print('height:', h)
-    ### ... IMAGE CODE ... ###
+    # These valuse are reversed intentionally to make sense physically.
 except IOError as e:
-    print(e)
+    exit(e)
