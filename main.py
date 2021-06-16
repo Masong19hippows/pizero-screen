f113d3941f6421970023a02eea4055e76dc254e2
Mason Garten <mgarten@sctelcom.com>
2021-06-16 13:27:27 -0500

    Testing

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ body = ImageFont.truetype("Roboto-Black.ttf", 18)
 #Initlizing 2.13 Display
 try:
     # Display init, clear
-    epd = epd2in13b_V3
+    epd = epd2in13b_V3.EPD()
     image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(image)
     draw.rectangle((0, 10, 200, 34), fill = 0)
@@ -18,6 +18,4 @@ except IOError as e:
     exit(e)
 
 def main():
-    image = Image.new(mode='1', size=(w, h), color=255)
-    draw = ImageDraw.Draw(image)
-    draw.text((0, 0), 'Coin: ', font=body, fill=0, align='left')
+    test = "test"
