af66a85f5dee6b3a48f1917b3a9ab174710e5ec1
Mason Garten <mgarten@sctelcom.com>
2021-06-16 14:53:10 -0500

    Testing

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ display.init()
 
 HBlackImage = Image.new('1', (epd2in13bc.EPD_HEIGHT, epd2in13bc.EPD_WIDTH), 255)
 HRedImage = Image.new('1', (epd2in13bc.EPD_HEIGHT, epd2in13bc.EPD_WIDTH), 255)
-draw2 = ImageDraw.Draw(HRedImage)
 draw = ImageDraw.Draw(HBlackImage)
 draw.text((0, 0), str(binance.price()), font = body, fill = 0)
-draw2.paste(Image.open('pic/binance.png'), (255, 0))
+HRedImage.paste(Image.open('pic/binance.png'), (255, 0))
 display.display(display.getbuffer(HBlackImage), display.getbuffer(HRedImage))
 
 def main():
