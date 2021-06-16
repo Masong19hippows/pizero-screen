7abb3caf7c9da6fcb888603c17dcf1de50b2b574
Mason Garten <mgarten@sctelcom.com>
2021-06-16 13:25:44 -0500

    Testing

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import os
-import getbinance as binance
-import getameritrade as ameritrade
+# import getbinance as binance
+# import getameritrade as ameritrade
 from waveshare_epd import epd2in13b_V3
 from PIL import Image, ImageDraw, ImageFont
 
 pic_dir = 'pic' # Points to pic directory.
-body = ImageFont.truetype("Roboto-Black.ttf", 18, index=5)
+body = ImageFont.truetype("Roboto-Black.ttf", 18)
 
 #Initlizing 2.13 Display
 try:
     # Display init, clear
-    display = epd2in13b_V3.EPD()
-    display.init(display.lut_full_update)
-    display.Clear(0) # 0: Black, 255: White
-    w = display.height
-    h = display.width
-    image = Image.new(mode='1', size=(w, h), color=255)
+    epd = epd2in13b_V3
+    image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((0, 10, 200, 34), fill = 0)
 except IOError as e:
     exit(e)
 
 def main():
     image = Image.new(mode='1', size=(w, h), color=255)
     draw = ImageDraw.Draw(image)
-    draw.text((0, 0), 'Coin: ' + binance.price(), font=body, fill=0, align='left')
+    draw.text((0, 0), 'Coin: ', font=body, fill=0, align='left')
