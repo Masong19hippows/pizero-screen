978f2970bd8ce8d9d9b7a8f2ea46d10ba773ffb4
Mason Garten <mgarten@sctelcom.com>
2021-06-16 15:17:27 -0500

    Working

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import getameritrade as ameritrade
 from waveshare_epd import epd2in13bc
 from PIL import Image, ImageDraw, ImageFont
 
+# Creates font and points to pic directory
 body = ImageFont.truetype("pic/Roboto-Black.ttf", 18)
-pic_dir = 'pic' # Points to pic directory .
+pic_dir = 'pic' 
+
 #Initlizing 2.13 Display
 # Display init, clear
 display = epd2in13bc.EPD()
 display.init()
 
+# Creating variables to controll screen and clearing screen
 HBlackImage = Image.new('1', (epd2in13bc.EPD_HEIGHT, epd2in13bc.EPD_WIDTH), 255)
 HRedImage = Image.new('1', (epd2in13bc.EPD_HEIGHT, epd2in13bc.EPD_WIDTH), 255)
-draw = ImageDraw.Draw(HBlackImage)
-draw.text((0, 0), str(binance.price()), font = body, fill = 0)
-HRedImage.paste(Image.open('pic/binance.png'), (255, 0))
 display.display(display.getbuffer(HBlackImage), display.getbuffer(HRedImage))
 
 def main():
     test = "test"
+    draw = ImageDraw.Draw(HBlackImage)
