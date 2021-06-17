3f40cd8bf25dbcbd471bcd916fff067a37de4ef7
Mason Garten <mgarten@sctelcom.com>
2021-06-17 14:52:15 -0500

    testing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import os
 import time
 import getbinance as binance
 import getameritrade as ameritrade
@@ -20,15 +19,19 @@ HRedImage = Image.new('1', (epd2in13bc.EPD_HEIGHT, epd2in13bc.EPD_WIDTH), 255)
 display.display(display.getbuffer(HBlackImage), display.getbuffer(HRedImage))
 time.sleep(5)
 
+# Making variables with prices to save on API calls
 binancePercent = binance.percent()
 ameritradePercent = ameritrade.percent()
 drawBlack = ImageDraw.Draw(HBlackImage)
 drawRed = ImageDraw.Draw(HRedImage)
 
+# Writing the base outline and making a future copy of it or future use
 drawBlack.text((0, 0), "Binance: $",font=body, fill=0, align='left')
 drawBlack.text((0, 50), "Ameritrade: $", font=body, fill=0, align='left')
 ogBlack = HBlackImage.copy()
 ogRed = HRedImage.copy()
+
+# Drawing The percentages on diffrent line
 drawBlack.text((95, 0), binance.price(),font=body, fill=0, align='left')
 drawBlack.text((126, 50), ameritrade.price(), font=body, fill=0, align='left')
 if float(binancePercent) < 0:
@@ -41,34 +44,42 @@ if float(ameritradePercent) < 0:
 else:
     drawBlack.text((115, 75), "+" + str(ameritradePercent) + "%",font=body, fill=0, align='left')
 
+# Writing to display
 display.display(display.getbuffer(HBlackImage), display.getbuffer(HRedImage))
 
 def updateDisplay():
+    
+# Restoring from copy made earlier
     HBlackImage = ogBlack.copy()
     HRedImage = ogRed.copy()
     drawBlack = ImageDraw.Draw(HBlackImage)
     drawRed = ImageDraw.Draw(HRedImage)
 
+# Updating Prices for ameritrade and binance to screen
     drawBlack.text((95, 0), binance.price(),font=body, fill=0, align='left')
     drawBlack.text((126, 50), ameritrade.price(), font=body, fill=0, align='left')
-
+    
+# Updating binance percent to screen
     binancePercent = binance.percent()
     if float(binancePercent) < 0:
         drawRed.text((115, 25), binancePercent + "%",font=body, fill=0, align='left')
     else:
         drawBlack.text((115, 25), "+" + binancePercent + "%",font=body, fill=0, align='left')
-    
+
+# Updating ameritrade percent to screen
     ameritradePercent = ameritrade.percent()
     if float(ameritradePercent) < 0:
         drawRed.text((115, 75), ameritradePercent + "%",font=body, fill=0, align='left')
     else:
         drawBlack.text((115, 75), "+" + ameritradePercent + "%",font=body, fill=0, align='left')
 
+# Displaying changes to screen
     display.display(display.getbuffer(HBlackImage), display.getbuffer(HRedImage))
 
-    time.sleep(29)
+# Adding countdown timer untill next exec
+    time.sleep(1 * 60)
 
+# Creates a loop to update the display
 while True:
-    time.sleep(1)
     updateDisplay()
     
--- setup.sh
+++ setup.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# if  [[ $1 = "-v" ]]; then
+#     echo "Option -v turned on"
+#     echo "Updating and instaling required packages..."
+#     apt-get update && apt-get upgrade -y
+#     sleep 2
+#     apt-get install python3-pip python3-pil python3-numpy -y
+#     sleep 2
+#     python3 -m pip install RPi.GPIO spidev python-dotenv python-binance td-ameritrade-python-api
+#     sleep 2
+#     echo "All Done"
+# else
+#     echo "Option -v Turned off"
+#     echo "Updating and instaling required packages..."
+#     apt-get update > /dev/null 2>&1 && apt-get upgrade -y > /dev/null 2>&1
+#     sleep 2
+#     apt-get install python3-pip python3-pil python3-numpy -y > /dev/null 2>&1
+#     sleep 2
+#     python3 -m pip install RPi.GPIO spidev python-dotenv python-binance td-ameritrade-python-api > /dev/null 2>&1
+#     sleep 2
+#     echo "All Done!"
+# fi
 
-if  [[ $1 = "-v" ]]; then
-    echo "Option -v turned on"
-    echo "Updating and instaling required packages..."
-    apt-get update && apt-get upgrade -y
-    sleep 2
-    apt-get install python3-pip python3-pil python3-numpy -y
-    sleep 2
-    python3 -m pip install RPi.GPIO spidev python-dotenv python-binance td-ameritrade-python-api
-    sleep 2
-    echo "All Done"
-else
-    echo "Option -v Turned off"
-    echo "Updating and instaling required packages..."
-    apt-get update > /dev/null 2>&1 && apt-get upgrade -y > /dev/null 2>&1
-    sleep 2
-    apt-get install python3-pip python3-pil python3-numpy -y > /dev/null 2>&1
-    sleep 2
-    python3 -m pip install RPi.GPIO spidev python-dotenv python-binance td-ameritrade-python-api > /dev/null 2>&1
-    sleep 2
-    echo "All Done!"
-fi
-
-#Get API key and Secret and add into .env file
-if test -f creds/.env; then
-    read -p 'Do you wish to overwite existing Keys file? Type "Y" or "N":' yn
-    case $yn in
-    [Yy]* ) echo "Type in your Binance api key:"; read binanceKey; echo "Type in your Binance api secret:"; read binanceSecret; echo "Type in your Ameritrade consumer key"; read consumerKey; printf "binanceKey=$binanceKey\nbinanceSecret=$binanceSecret\nconsumerKey=$consumerKey" > creds/.env; exit;;
-    [Nn]* ) exit;;
-    esac
-else
-    echo "Type in your Binance api key:"
-    read binanceKey
-    echo "Type in your Binance api secret:"
-    read binanceSecret
-    echo "Type in your Ameritrade consumer key"
-    read consumerKey
-    printf "binanceKey=$binanceKey\nbinanceSecret=$binanceSecret\nconsumerKey=$consumerKey" > creds/.env
-    exit
-fi
+# #Get API key and Secret and add into .env file
+# if test -f creds/.env; then
+#     read -p 'Do you wish to overwite existing Keys file? Type "Y" or "N":' yn
+#     case $yn in
+#     [Yy]* ) echo "Type in your Binance api key:"; read binanceKey; echo "Type in your Binance api secret:"; read binanceSecret; echo "Type in your Ameritrade consumer key"; read consumerKey; printf "binanceKey=$binanceKey\nbinanceSecret=$binanceSecret\nconsumerKey=$consumerKey" > creds/.env; exit;;
+#     [Nn]* ) exit;;
+#     esac
+# else
+#     echo "Type in your Binance api key:"
+#     read binanceKey
+#     echo "Type in your Binance api secret:"
+#     read binanceSecret
+#     echo "Type in your Ameritrade consumer key"
+#     read consumerKey
+#     printf "binanceKey=$binanceKey\nbinanceSecret=$binanceSecret\nconsumerKey=$consumerKey" > creds/.env
+#     exit
+# fi
+echo "$DIR"
+# printf "@reboot pi bash  $DIR/main.py" > /etc/cron.d/screen
+sleep(20)
